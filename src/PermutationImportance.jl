5e9f66877d86f4a587535d4803b80e51ba4f77b3
charizard1ash <nathan.s.simonson@gmail.com>
2020-07-12 15:32:43 +1000

    module update

--- src/PermutationImportance.jl
+++ src/PermutationImportance.jl
@@ -1,18 +1,21 @@
 module PermutationImportance
 
-"""
-    permutation_importance(;
-                           )
-    
-    This package is an implementation of permutation importance,
-    and is applicable to any machine learning model using a
-    DataFrames data input, and a model prediction function.
-"""
-
-## Load libraries
 using DataFrames
 using Random
 
 export permutation_importance
+
+"""
+permutation_importance(;df::DataFrames.DataFrame,
+                       label::String,
+                       model,
+                       predict_function::Function,
+                       loss=[:loglos_1, :logloss_cat, :accuracy, :rmse],
+                       n_perm::Int64=20,
+                       parallel=[0, 1])
+
+Run permutation importance algorithm to determine which attributes have the most significant impact when chaos is incorporated into the model.
+This algorithm is built to be model agnostic.
+"""
 
 end
