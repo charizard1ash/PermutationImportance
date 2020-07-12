c795a529449750ffa71f143674f7f8ba125bbf9f
charizard1ash <nathan.s.simonson@gmail.com>
2020-07-12 15:14:41 +1000

    initial commit

--- src/PermutationImportance.jl
+++ src/PermutationImportance.jl
@@ -1,3 +1,18 @@
 module PermutationImportance
 
+"""
+    permutation_importance(;
+                           )
+    
+    This package is an implementation of permutation importance,
+    and is applicable to any machine learning model using a
+    DataFrames data input, and a model prediction function.
+"""
+
+## Load libraries
+using DataFrames
+using Random
+
+export permutation_importance
+
 end
